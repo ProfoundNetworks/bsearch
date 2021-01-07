3fe75f973e830acab0734c2cdf1282be5d10ec78
Gavin Carr <gavin@profound.net>
2021-01-08 09:37:42 +1100

    Add first-pass bsearch_index utility.

---
+++ cmd/bsearch_index/bsearch_index.go
@@ -0,0 +1,80 @@
+/*
+bsearch utility to generate a bsearch index file for a dataset.
+The index file has the same name and location as the dataset,
+but with a '.bsx' suffix (although it is just a yaml text file).
+*/
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+
+	"github.com/ProfoundNetworks/bsearch"
+	flags "github.com/jessevdk/go-flags"
+)
+
+// Options
+var opts struct {
+	Verbose bool `short:"v" long:"verbose" description:"display verbose debug output"`
+	Force   bool `short:"F" long:"force" description:"force index generation even if up-to-date"`
+	Args    struct {
+		Filename string
+	} `positional-args:"yes" required:"yes"`
+}
+
+// Disable flags.PrintErrors for more control
+var parser = flags.NewParser(&opts, flags.Default&^flags.PrintErrors)
+
+func usage() {
+	parser.WriteHelp(os.Stderr)
+	os.Exit(2)
+}
+
+func vprintf(format string, args ...interface{}) {
+	if opts.Verbose {
+		fmt.Fprintf(os.Stderr, format, args...)
+	}
+}
+
+func main() {
+	// Parse options
+	_, err := parser.Parse()
+	if err != nil {
+		if flagsErr, ok := err.(*flags.Error); ok && flagsErr.Type != flags.ErrHelp {
+			fmt.Fprintf(os.Stderr, "%s\n\n", err)
+		}
+		usage()
+	}
+
+	// Setup
+	log.SetFlags(0)
+
+	// Die if Filename looks compressed
+	reCompression := regexp.MustCompile(`\.(gz|bz2|zst|br)$`)
+	if reCompression.MatchString(opts.Args.Filename) {
+		fmt.Fprintf(os.Stderr, "Filename %q appears to be compressed - cannot binary search\n", opts.Args.Filename)
+		os.Exit(2)
+	}
+
+	// Noop if a valid index already exists
+	if !opts.Force {
+		_, err = bsearch.NewIndexLoad(opts.Args.Filename)
+		if err == nil {
+			vprintf("+ index file found and up to date\n")
+			os.Exit(0)
+		}
+	}
+
+	// Generate and write index
+	index, err := bsearch.NewIndex(opts.Args.Filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = index.Write()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
--- index.go
+++ index.go
@@ -5,7 +5,10 @@ Index provides an optional bsearch index implementation.
 package bsearch
 
 import (
+	"bytes"
 	"errors"
+	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -31,6 +34,7 @@ type Index struct {
 	Delimiter string       `yaml:"delim"`
 	Epoch     int64        `yaml:"epoch"`
 	Filename  string       `yaml:"filename"`
+	Filedir   string       `yaml:"filedir"`
 	Header    bool         `yaml:"header"`
 	List      []IndexEntry `yaml:"list"`
 }
@@ -43,18 +47,138 @@ func epoch(filename string) (int64, error) {
 	return stat.ModTime().Unix(), nil
 }
 
-// NewIndex creates a new Index for filename from scratch.
-//func NewIndex(filename string) (*Index, error) {
-//}
+// processBlock processes the block in buf[:bytesread] and returns an IndexEntry
+// for the first line it finds.
+func processBlock(buf []byte, bytesread int, blockPosition int64, delim string, firstBlock, prevNL bool) (IndexEntry, error) {
+	// Find first newline
+	nlidx := -1
+	if !prevNL {
+		nlidx = bytes.IndexByte(buf[:bytesread], '\n')
+		if nlidx == -1 {
+			// Corner case - no newline found in block
+			return IndexEntry{}, errors.New("Missing nlidx handling not yet implemented")
+		}
+	}
+
+	// Find delimiter
+	didx := bytes.IndexByte(buf[nlidx+1:bytesread], byte(delim[0]))
+	if didx == -1 {
+		// Corner case - no delimiter found in block
+		return IndexEntry{}, fmt.Errorf("Missing didx handling not yet implemented (nlidx %d):\n%s\n", nlidx, string(buf[nlidx+1:bytesread]))
+	}
+	didx += nlidx + 1
+
+	// Check that there's no newline in this chunk
+	if nlidx2 := bytes.IndexByte(buf[nlidx+1:didx], '\n'); nlidx2 != -1 {
+		return IndexEntry{}, fmt.Errorf("Error: line without delimiter found:\n%s\n", string(buf[nlidx+1:nlidx2]))
+	}
+
+	// Create entry
+	entry := IndexEntry{}
+	entry.Key = string(buf[nlidx+1 : didx])
+	entry.Offset = blockPosition + int64(nlidx) + 1
+
+	// On the first block only, check for the presence of a header
+	if firstBlock {
+		nlidx = bytes.IndexByte(buf[:bytesread], '\n')
+		if nlidx == -1 {
+			// Corner case - no newline found in block
+			return IndexEntry{}, errors.New("Missing nlidx handling (firstBlock) not yet implemented")
+		}
+		entry2, err := processBlock(buf[nlidx+1:bytesread], bytesread-(nlidx+1), blockPosition+int64(nlidx+1), delim, false, true)
+		if err != nil {
+			return IndexEntry{}, err
+		}
+		// If the entry.Key > entry2.Key, assume the first is a header
+		if entry.Key > entry2.Key {
+			return entry2, nil
+		}
+	}
+
+	return entry, nil
+}
+
+// IndexFile returns the basename of the index file associated with filename
+func IndexFile(filename string) string {
+	reNonSuffix := regexp.MustCompile(`^[^.]+`)
+	matches := reNonSuffix.FindStringSubmatch(filepath.Base(filename))
+	idxfile := matches[0] + "." + indexSuffix
+	return idxfile
+}
+
+// NewIndex creates a new Index for the filename dataset from scratch
+func NewIndex(filename string) (*Index, error) {
+	reader, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	epoch, err := epoch(filename)
+	if err != nil {
+		return nil, err
+	}
+	filedir, err := filepath.Abs(filepath.Dir(filename))
+	if err != nil {
+		return nil, err
+	}
+
+	index := Index{}
+	// index.Collation = "C"
+	index.Delimiter = ","
+	index.Epoch = epoch
+	index.Filename = filepath.Base(filename)
+	index.Filedir = filedir
+	index.Header = false
+
+	// Process dataset in blocks
+	buf := make([]byte, defaultBlocksize)
+	list := []IndexEntry{}
+	var blockPosition int64 = 0
+	firstBlock := true
+	prevNL := true
+	prevKey := ""
+	for {
+		bytesread, err := reader.ReadAt(buf, blockPosition)
+		if err != nil && err != io.EOF {
+			return nil, err
+		}
+		if bytesread > 0 {
+			entry, err := processBlock(buf, bytesread, blockPosition, index.Delimiter, firstBlock, prevNL)
+			if err != nil {
+				return nil, err
+			}
+			// Check that all entry keys are sorted as we expect
+			if prevKey < entry.Key {
+				list = append(list, entry)
+			} else if prevKey > entry.Key {
+				return nil, fmt.Errorf("Error: key sort violation - %q > %q\n", prevKey, entry.Key)
+			}
+			prevKey = entry.Key
+			blockPosition += int64(bytesread)
+			// If the first offset is not zero we've skipped a header
+			if firstBlock && entry.Offset > 0 {
+				index.Header = true
+			}
+		}
+		if err != nil && err == io.EOF {
+			break
+		}
+		prevNL = false
+		if buf[bytesread-1] == '\n' {
+			prevNL = true
+		}
+		firstBlock = false
+	}
+
+	// Output
+	index.List = list
+	return &index, nil
+}
 
 // NewIndexLoad loads Index from the associated index file for filename.
 // Returns ErrNotFound if no index file exists.
 // Returns ErrIndexExpired if filename is newer than the index file.
 func NewIndexLoad(filename string) (*Index, error) {
-	reNonSuffix := regexp.MustCompile(`^[^.]+`)
-	matches := reNonSuffix.FindStringSubmatch(filepath.Base(filename))
-	idxfile := matches[0] + ".bsx"
-	idxpath := filepath.Join(filepath.Dir(filename), idxfile)
+	idxpath := filepath.Join(filepath.Dir(filename), IndexFile(filename))
 
 	_, err := os.Stat(idxpath)
 	if err != nil {
@@ -116,4 +240,18 @@ func (i *Index) BlockPosition(b []byte) (int64, error) {
 	}
 
 	return list[begin].Offset, nil
+}
+
+// Write writes the index to disk
+func (i *Index) Write() error {
+	data, err := yaml.Marshal(i)
+	if err != nil {
+		return err
+	}
+	idxpath := filepath.Join(i.Filedir, IndexFile(i.Filename))
+	err = ioutil.WriteFile(idxpath, data, 0644)
+	if err != nil {
+		return err
+	}
+	return nil
 }
